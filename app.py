465c9ca02ee3f641c56fd7787f2e6213a3f285c9
Mikegit config --global user.email mikmercieca13@gmail.com <101655537+Mike-Mercieca@users.noreply.github.com>
2022-09-21 14:15:39 +0100

    add code to include running db

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template, request, flash, redirect, url_for
 import sqlite3
 from hashids import Hashids
+import os.path
+import init_db 
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+db_path = os.path.join(BASE_DIR, "database.db")
 
 
 def get_db_connection():
-    connection = sqlite3.connect('database.db')
+    connection = sqlite3.connect(db_path)
     connection.row_factory = sqlite3.Row
     return connection
 
@@ -18,6 +23,8 @@ hashids = Hashids(min_length=4, salt=app.config['SECRET_KEY'])
 @app.route('/', methods =['GET', 'POST'])
 def index():
     connection = get_db_connection()
+    print(connection)
+    print(db_path)
 
     if request.method == 'POST':
         url = request.form['url']
@@ -32,17 +39,8 @@ def index():
         return render_template('index.html', short_url = short_url)
     
     return render_template('index.html')
-        
-
-
-
-
-
-
-
-
-
-
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
+    init_db.run()
